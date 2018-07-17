6886d5237908aa63fbffc5f1ab54580c83c8105a
Siddharth Vadgama <siddv@hotmail.com>
2018-07-17 19:46:48 +0100

    publish to port 80

--- index.js
+++ index.js
@@ -42,4 +42,4 @@ Rolls: *${randchar.rollTotals.join('*, *')}*`
   })
 })
 
-app.listen(3000, () => console.log('Randchar-slack listening on port 3000!'))
+app.listen(80, () => console.log('Randchar-slack listening on port 3000!'))
